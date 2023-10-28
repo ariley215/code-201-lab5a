db88be47ed7b11526ed305c38cbf3d8001500e85
Andrea Riley <andreariley@me.com>
2023-10-27 21:01:05 -0400

    problem 3 passes

--- js/app.js
+++ js/app.js
@@ -55,10 +55,15 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
 
-}
+  let result = sum(a,b)[0];
+  result = sum(result,c)[0];
+  let diffResult = multiply(a,b)[0];
+  diffResult = multiply(diffResult,c)[0];
 
+  return [result, diffResult,`${a} and ${b} and ${c} sum to ${result}.`, `The product of ${a} and ${b} and ${c} is ${diffResult}.`];
+}
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
